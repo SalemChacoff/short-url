061ebcc083a62bb2fb3b2ecfaf8a4a398883bbbf
SalemChacoff <77862771+SalemChacoff@users.noreply.github.com>
2025-07-26 22:19:45 -0400

    Feat: Initialize Spring Boot application with Gradle build configuration and basic structure

---
+++ listener-api/src/main/java/com/java/consumer/ListenerApiApplication.java
@@ -0,0 +1,13 @@
+package com.java.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListenerApiApplication.class, args);
+    }
+
+}
